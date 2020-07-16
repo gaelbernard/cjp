4ce2f3c2a06438914c807e277862de0c9dc5fa7d
Gaël Bernard <gael.bernard@unil.ch>
2020-07-16 11:20:35 +0200

    Described transformation

--- datasets/transform.py
+++ datasets/transform.py
@@ -1,53 +1,47 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from matplotlib import pyplot as plt
+
+'''
+The dataset XXX contains confidential information about ticket handling in a service desk of a municipality over a time span of one year. The goal of this file is to anonymzed the dataset and only keep the top $1\%$ of customers that generated most of the tickets throughout the year. By doing so, we focus the experiment on the difficult cases where many tickets are generated within a short period of time. It results in a customer journey event log with the following characteristics: 27 customer journeys, 7.9K tickets, and 100.9K `touchpoints' of 290 types.
+'''
 
 dataset = {
-    'csv_path':'../datasets/XXX.csv',
-    'name':'sil',
-    'group_by': ' Demandeur',
-    'activity_col':' Libelle_intervenant',
-    'time_col':" Date_de_fin_de_l'action",
-    'guess_col':'N',
+    'csv_path':'XXX.csv',
+    'name':'XXX',
+    'group_by': ' XXX',
+    'activity_col':' XXX',
+    'time_col':" XXX",
+    'guess_col':'XXX',
     'sep':';',
     'experiment':'real_dataset'
 }
-df = pd.read_csv('sil.csv', sep=';', nrows=None)
+df = pd.read_csv(dataset['csv_path'], sep=dataset['sep'])
 
 time_col = dataset['time_col']
 group_by_col = dataset['group_by']
 activity_col = dataset['activity_col']
 guess_col = dataset['guess_col']
 
-print (df.shape)
+# PREPROCESSING
+# interpret datetime
+df[time_col] = pd.to_datetime(df[time_col])
 
+# sort values
 df.sort_values(by=[group_by_col, time_col], inplace=True)
-print (df.head().to_string())
 
+# removing lines with nan values
 for v in [guess_col, group_by_col, time_col, activity_col]:
     df = df[df[v].notna()]
 
-df[time_col] = pd.to_datetime(df[time_col])
-
-# Group by day
-#df[group_by_col] = df[group_by_col].astype('str') +'_'+ pd.to_datetime(df[time_col]).dt.date.astype('str')
-
+# TRANSFORM
 # Keep only busy CJs
-print (df.shape)
 nlarg = df.groupby(group_by_col)[guess_col].nunique()
-print (nlarg[nlarg>nlarg.quantile(0.99)].to_string())
 df = df[df[group_by_col].isin(nlarg[nlarg>nlarg.quantile(0.99)].index)]
 
-
-#print (nlarg.to_string())
-print (df.shape)
-
-# Transform to timestamp
+# Scale the timestamp
 df[time_col] = df[time_col].astype(int)/ 10**9
 df['time'] = df[time_col]
-print (df.columns)
-
 df['ticket_id'] = df['N'].map({k:y for y,k in pd.Series(df['N'].unique()).items()})
 df['customer_id'] = df[' Demandeur'].map({k:y for y,k in pd.Series(df[' Demandeur'].unique()).items()})
 df['activity'] = df[' Libelle_intervenant'].map({k:y for y,k in pd.Series(df[' Libelle_intervenant'].unique()).items()})
@@ -56,8 +50,3 @@ df['time_diff_norm'] = np.log(df['time_diff']+1)
 df['time_diff_norm'] = MinMaxScaler().fit_transform(df['time_diff_norm'].values.reshape(-1,1))
 df = df[['ticket_id', 'customer_id', 'activity', 'time', 'time_diff', 'time_diff_norm']]
 df.to_csv('real_transformed.csv')
-
-exit()
-df['time_diff_norm'].plot.hist(bins=100)
-plt.show()
-
